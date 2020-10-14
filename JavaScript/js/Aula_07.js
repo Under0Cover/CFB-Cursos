98317477c0e1752a1eecba75efae6a7905d8e333
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:11:07 -0300

    comando if
    Aula 07 - Curso de JavaScript

---
+++ JavaScript/js/Aula_07.js
@@ -0,0 +1,14 @@
+// Comando IF - ELSE
+// if (teste lógico){comandos}
+if (10 > 5){
+    document.write("CFB Cursos")
+}
+if (10 < 5){
+    document.write("Curso de JavaScript")
+}
+var valor_01, valor_02
+valor_01 = 10
+valor_02 = 5
+if (valor_01 > valor_02){
+    document.write(" - Curso de Programação")
+}
