53ef9b22b50b5a8bf145dfe1f01dc173b4f267f5
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:22:12 -0300

    Comando do-while
    Aula 20 - Curso de JavaScript

---
+++ JavaScript/js/Aula_20.js
@@ -0,0 +1,13 @@
+// Comando Do-While
+var numero = 20
+
+while (numero < 10){
+    document.write("CFB Cursos <br>")
+    numero++
+}
+
+document.write("<hr>")
+
+do {
+    document.write("CFB Cursos <br>")
+} while (numero < 10)
