90d7104a4664cee87583c9340959a23e6aa046c5
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:22:52 -0300

    Declaração de variável com let
    Aula 21 - Curso de JavaScript

---
+++ JavaScript/js/Aula_21.js
@@ -0,0 +1,31 @@
+// Declaração de variáveis com let
+
+var numero = 10
+var numero_01 = 10
+let numero_02 = 10
+let numero_03 = 10
+
+document.write(numero + "<br>")
+document.write(numero_01 + "<br>")
+document.write(numero_02 + "<br>")
+document.write(numero_03 + "<br>")
+document.write("<hr>")
+
+
+if (numero == 10){
+    numero = 5
+    let numero_01 = 5
+    numero_02 = 5
+    let numero_03 = 5
+    document.write(numero + "<br>")
+    document.write(numero_01 + "<br>")
+    document.write(numero_02 + "<br>")
+    document.write(numero_03 + "<br>")
+    document.write("<hr>")
+}
+
+document.write(numero + "<br>")
+document.write(numero_01 + "<br>")
+document.write(numero_02 + "<br>")
+document.write(numero_03 + "<br>")
+document.write("<hr>")
