21bd90855df478aa3ba6f7e126ae1d38248b4756
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:08:22 -0300

    var e const
    Aula 04 - Curso de JavaScript

---
+++ JavaScript/js/Aula_04.js
@@ -0,0 +1,11 @@
+var canal = "CFB Cursos"
+var curso = "Curso de JavaScript"
+window.alert("CFB Cursos - Curso de JavaScript")
+alert(canal + " - " + curso)
+var curso = "Curso de Programação Web"
+alert(canal + " - " + curso)
+alert(200 + 100)
+const num = 500
+alert(num + 300)
+const separador = " - "
+alert(canal + separador + curso)
