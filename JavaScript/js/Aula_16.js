d3de92ac37751ae4e9002c28ac7d051d04d3a9d2
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:19:52 -0300

    Matrizes
    Aula 16 - Curso de JavaScript

---
+++ JavaScript/js/Aula_16.js
@@ -0,0 +1,29 @@
+// Matrizes
+var mochila = []
+
+var item_01 = ["Corda", 02]
+var item_02 = ["Faca", 01]
+var item_03 = ["Cura", 05]
+var item_04 = ["Prego", 50]
+
+mochila.push(item_01)
+mochila.push(item_02)
+mochila.push(item_03)
+mochila.push(item_04)
+
+document.write(mochila[0][0] + " -> " + mochila[0][1] + "<br>")
+document.write(mochila[1][0] + " -> " + mochila[1][1] + "<br>")
+document.write(mochila[2][0] + " -> " + mochila[2][1] + "<br>")
+document.write(mochila[3][0] + " -> " + mochila[3][1] + "<br>")
+
+document.write("<br>")
+document.write(mochila[0])
+document.write("<br>")
+document.write(mochila[1])
+document.write("<br>")
+document.write(mochila[2])
+document.write("<br>")
+document.write(mochila[3])
+
+
+
