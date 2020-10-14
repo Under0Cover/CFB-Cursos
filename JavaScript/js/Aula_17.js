6b70a267c39c4631b4b27c7324c6dd1e38ab6d3b
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:20:29 -0300

    Incremento e Decremento de Variáveis
    Aula 17 - Curso de JavaScript

---
+++ JavaScript/js/Aula_17.js
@@ -0,0 +1,42 @@
+// Incremento e Decremento de Variáveis
+var numero = 10
+document.write(numero)
+document.write("<br>")
+
+numero += 9
+document.write(numero)
+document.write("<br>")
+
+numero -= 5
+document.write(numero)
+document.write("<br>")
+
+numero *= 3
+document.write(numero)
+document.write("<br>")
+
+numero /= 2
+document.write(numero)
+document.write("<br>")
+
+document.write(numero++)
+document.write("<br>")
+document.write(numero)
+document.write("<br>")
+document.write(++numero)
+document.write("<br>")
+document.write(numero)
+document.write("<br>")
+
+numero *= -1
+document.write(numero)
+document.write("<br>")
+
+numero *= -1
+document.write(numero)
+document.write("<br>")
+
+document.write(-numero)
+document.write("<br>")
+document.write(numero)
+document.write("<br>")
