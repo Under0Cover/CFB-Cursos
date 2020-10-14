298f7e4925490396806c9aabaf6407672ece7431
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:07:12 -0300

    Variáveis
    Aula 02 - Curso de JavaScript

---
+++ JavaScript/js/Aula_02.js
@@ -0,0 +1,18 @@
+var canal = "CFB Cursos"
+var nome, sobrenome, idade, endereco, site;
+document.write(nome);
+var numero = null;
+var ano = 2020;
+document.write(canal);
+document.write(ano);
+document.write("<br>");
+document.write(canal + "<br>" + ano);
+curso = "JavaScript";
+document.write("<br>" + curso + "<br>");
+curso = "Programação";
+document.write(curso);
+numero = 150;
+var texto = canal + "<br>" + ano + numero;
+var texto_01 = canal + "<br>" + (ano + numero);
+document.write(texto);
+document.write(texto_01);
