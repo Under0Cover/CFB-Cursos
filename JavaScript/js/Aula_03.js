2841292063350b167817d06ef68acddefcd12ce1
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:07:49 -0300

    Variáveis - Parte 02
    Aula 03 - Curso de JavaScript

---
+++ JavaScript/js/Aula_03.js
@@ -0,0 +1,8 @@
+var canal = "CFB Cursos"
+document.write(canal)
+canal = "JavaScript"
+document.write(canal)
+const canal_01 = "CFB Cursos"
+document.write(canal_01)
+// canal_01 = "JavaScript" // Aqui dá ERRO
+document.write(Math.PI)
