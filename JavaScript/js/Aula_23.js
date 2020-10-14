cbec21a61906bdc41c47cacb82ef3bda2ed4e44e
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:24:01 -0300

    getElementByID
    Aula 23 - Curso de JavaScript

---
+++ JavaScript/js/Aula_23.js
@@ -0,0 +1,11 @@
+// getElementById
+var texto = document.getElementById("texto")
+texto.style.color = "#f00"
+texto.style.fontSize = "50px"
+texto.style.display = "inline"
+var div = document.getElementById("caixa")
+caixa.style.border = "1px solid #000"
+caixa.style.width = "300px"
+caixa.style.height = "300px"
+caixa.style.backgroundColor = "#aaa"
+caixa.style.display = "block"
