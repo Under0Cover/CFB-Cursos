357a68bd0da33db913e78344979fcf330cd37f03
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:08:58 -0300

    promt
    Aula 05 - Curso de JavaScript

---
+++ JavaScript/js/Aula_05.js
@@ -0,0 +1,5 @@
+var nome = prompt("Digite seu nome:", "Seu nome aqui...")
+curso = "JavaScript"
+var sobrenome = prompt("Digite seu sobrenome: ", curso)
+document.write(nome)
+document.write(sobrenome)
