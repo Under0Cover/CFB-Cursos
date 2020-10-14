a88b36b3e53a6ee00d0f2f66b46086d4d5d5c2eb
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:16:29 -0300

    Array - Vetor (Push)
    Aula 13 - Curso de JavaScript

---
+++ JavaScript/js/Aula_13.js
@@ -0,0 +1,25 @@
+
+// Array - Vetor (Parte 01)
+// Metodo Push
+var cores = new Array()
+cores.push("Preto")
+cores.push("Branco")
+cores.push("Azul")
+cores.push("Vermelho")
+cores.push("Verde")
+document.write(cores)
+document.write("\n")
+
+var animais = []
+animais[0] = "Vaca"
+animais[1] = "Cavalo"
+animais[2] = "Cachorro"
+animais[3] = "Gato"
+animais[4] = "Pato"
+document.write(animais)
+document.write("\n")
+var fazenda = []
+fazenda.push(animais[1])
+fazenda.push(animais[4])
+fazenda.push(animais[0])
+document.write(fazenda)
