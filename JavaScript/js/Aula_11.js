7544436a07bce6fd626692e0b484eaaead312099
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:14:06 -0300

    switch case
    Aula 11 - Curso de JavaScript

---
+++ JavaScript/js/Aula_11.js
@@ -0,0 +1,11 @@
+// Bloco de comandos
+var numero = prompt("Digite um número: ")
+if(numero > 10){
+    resultado = "Maior que 10"
+    document.write(resultado)
+    alert(resultado)
+}else{
+    resultado = "Menor que 10"
+    document.write(resultado)
+    alert(resultado)
+}
