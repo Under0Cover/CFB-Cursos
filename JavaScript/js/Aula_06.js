64b6e57b2cefdee864c97ed0274f7dfb3c9eda1c
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:10:21 -0300

    prompt - true - false
    Aula 06 - Curso de JavaScript

---
+++ JavaScript/js/Aula_06.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite o seu nome:", "Nome aqui")
+var resultado = confirm(nome + " deseja continuar?")
+// OK -> Retorna True
+// Cancelar -> Retorna False
+alert(resultado)
+// document.write(resultado)
