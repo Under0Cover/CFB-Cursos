2ace3477fcde304da182007e4b1c77976fb66a0a
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-14 18:25:18 -0300

    getElementById - innerHTML
    Aula 24 - Curso de JavaScript

---
+++ JavaScript/js/Aula_24.js
@@ -0,0 +1,5 @@
+// getElementById e innerHTML
+var caixa_Texto_01 = document.getElementById("caixaTexto").value = "Curso de JavaScript"
+// alert(caixa_Texto_01)
+var texto = document.getElementById("texto").innerHTML = "Aula 24 - innerHTML"
+var div = document.getElementById("textoDiv").innerHTML = "Texto modificado pelo innerHTML na DIV"
